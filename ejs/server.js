4155d500a1049582a8c4e91e9ebc2d5d2093a4a3
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-16 11:00:21 -0700

    different string format

--- ejs/server.js
+++ ejs/server.js
@@ -3,13 +3,13 @@ const express = require('express'),
         port = process.argv[2] || 8080;
 
 app.listen(port, function(){
-    console.log("Server is running on port: " + port);
+    console.log(`Server is running on port: ${port}`);
 });
 
 app.get('/home', function(req, res){
     let heading = req.query.heading;
     let username = req.query.username;
-    let response = "<h1>" + heading + "</h1>";
-    response +=  "<h1>" + username + "</h1>";
+    let response = `<h1>${heading}</h1>`;
+    response +=  `<h1>${username}</h1>`;
     res.send(response);
 });
