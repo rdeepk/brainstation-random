cf91cebd3a91511d7810cd06158a7b8f4a7888f4
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-16 10:58:15 -0700

    express first draft

---
+++ ejs/server.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+        app = express(),
+        port = process.argv[2] || 8080;
+
+app.listen(port, function(){
+    console.log("Server is running on port: " + port);
+});
+
+app.get('/home', function(req, res){
+    let heading = req.query.heading;
+    let username = req.query.username;
+    let response = "<h1>" + heading + "</h1>";
+    response +=  "<h1>" + username + "</h1>";
+    res.send(response);
+});
