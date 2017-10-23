fac73f9b1af2a9bf8dd7105679b58d3371e634e3
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-23 11:55:02 -0700

    another revision

--- react_demo/src/index.js
+++ react_demo/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Hello from './Hello';
+import GoodBye from './GoodBye';
 import registerServiceWorker from './registerServiceWorker';
 
 const name = "Kenneth";
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Hello name={name}/>, document.getElementById('root'));
+ReactDOM.render(<Hello name={name}/>, document.getElementById('root1'));
+ReactDOM.render(<GoodBye firstName="Rk"
+                        lastName = "Deep"/>,
+                        document.getElementById('root2'));
 registerServiceWorker();
