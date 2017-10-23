854e1572571fa6824cf7e978ae221b6613ca550e
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-23 11:56:29 -0700

    another revision

--- react_demo/src/index.js
+++ react_demo/src/index.js
@@ -12,4 +12,7 @@ ReactDOM.render(<Hello name={name}/>, document.getElementById('root1'));
 ReactDOM.render(<GoodBye firstName="Rk"
                         lastName = "Deep"/>,
                         document.getElementById('root2'));
+ReactDOM.render(<GoodBye firstName="Nick"
+                        lastName = "Tamura"/>,
+                        document.getElementById('root3'));
 registerServiceWorker();
