ea9a13b723ea03a937b26a4ebf20a1508b21bbce
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-23 14:43:28 -0700

    include how to use other components into a component

--- react_demo/src/Greeting.js
+++ react_demo/src/Greeting.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Hello from './Hello';
+import GoodBye from './GoodBye';
 
 class Greeting extends React.Component {
     render() {
         let name = 'Jonathan'
         return (
             <div>
-                <h1>Hello {name}</h1>
-                <h1>GoodBye {name}</h1>
+                <Hello name={name} />
+                <GoodBye firstName='Nic' lastName='Tamura'/>
             </div>
         )
     }
