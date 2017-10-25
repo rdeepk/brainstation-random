d8bffbbd132a39639c086e8e45df6aae79eb1abf
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-25 11:16:40 -0700

    using checkboxes for state

--- react-to-do-demo/src/components/TodoList/index.jsx
+++ react-to-do-demo/src/components/TodoList/index.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import Todo from '../Todo';
 
 class TodoList extends Component {
     render(){
         let todosJSX = this.props.todos.map((todo, i) => {
             return(
-                <li key={i}>{todo.text}</li>
+               <Todo key={i}
+               text={todo.text}
+               done={todo.done} />
             )
         })
         return (
