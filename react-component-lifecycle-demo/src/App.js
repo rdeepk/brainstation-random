4215383cf3eeb6bcadac4a4a949ffa515e9a2146
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-25 14:42:13 -0700

    component lifecycle example

---
+++ react-component-lifecycle-demo/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      counter: 0
+    }
+    console.log("App::Constructor")
+  }
+
+  componentWillMount() {
+    console.log("will mount");
+  }
+
+  componentDidMount() {
+    console.log("did mount");
+  }
+  componentWillUpdate() {
+    console.log("will update");
+  }
+  componentDidUpdate() {
+    console.log("did update");
+  }
+
+  buttonHandler = () => {
+    this.setState({
+      counter:this.state.counter+1
+    })
+  }
+  render() {
+    console.log("App::Render");
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          counter: {this.state.counter}
+          <button type="button" onClick={this.buttonHandler}>Click</button> 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
