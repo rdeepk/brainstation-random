95dffa67e8b0a812d6bf4c428f2cc632fb34ca3e
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-31 10:41:34 -0700

    react routing using react-router-dom

--- routing-demo/src/index.js
+++ routing-demo/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './Home';
+import About from './About';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={App} /> //without exact keyword this route will match all the routes because every one has a / and this is the first one the router will encounter, so this will run every single time. To overcome that exact keyword is used.
+                    <Route path="/about" component={About} />
+                    <Route path="/home" component={Home} />
+                </Switch>
+            </Router>,
+            document.getElementById('root'));
 registerServiceWorker();
