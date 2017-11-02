2d0baae66107dd5315d2bc7fca4f4f50e958a193
Ramandeep Kaur <rdeepk@gmail.com>
2017-11-02 11:42:56 -0700

    use of dark sky api using react to display weather data

---
+++ react-weather-demo/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      city: '',
+      time: '',
+      temperature: '',
+      summary:''
+    }
+  }
+  componentWillMount() {
+    let promise = axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/94cd206408f85af008b3a9a3bd4bbdd8/49.2827, 123.1207')
+    promise.then((response) => {
+      console.log(response);
+      this.setState({
+        city:'',
+        time: response.data.currently.time,
+        temperature:response.data.currently.temperature,
+        summary: response.data.currently.summary
+      })
+    })
+  }
+
+   render() {
+    
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          City: <br />
+          Time: {this.state.time}<br />
+          Summary: {this.state.summary}<br />
+          Temperature: {this.state.temperature} F<br />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
